5d3f40811856c702069bfffd29b5de750ba46a5b
AggieGirl22 <johannahein@tamu.edu>
2021-10-15 15:31:27 -0500

    small edits

--- route_planning.py
+++ route_planning.py
@@ -1,11 +1,29 @@
 import pandas as pd
 
 
-def read_from_gui(mode, pixel_s):
-    # mode - which mode the user selected, string or int
+def read_from_gui(mode, guidf):
+    # mode - which mode the user selected, int
+    #   -> 1, 2-DSel
+    #   -> 2, 2-DTS
+    #   -> 3, 1-DTS
+    #   -> 4, RPA
+
     # pixel_s - pixel coordinate(s) from the sky map photo, list
 
-    x = 2
+    if mode == 1:
+        guidf.loc[3] = [guidf.iloc[0, 0], guidf.iloc[1, 1]]
+        guidf.loc[4] = [guidf.iloc[1, 0], guidf.iloc[0, 1]]
+        print(guidf)
+    elif mode == 2:
+        # hard code in starting line end points
+        endpoints = [[150, 300], [150, 0]]
+        guidf = pd.DataFrame(endpoints)
+        print(guidf)
+    elif mode == 3:
+        print(guidf)
+    elif mode == 4:
+
+        print(guidf)
 
 
 def earth_rotation():
@@ -18,3 +36,9 @@ def earth_rotation():
     #   - scan every 2/3 hour or 40 min
 
     y = 3
+
+
+gui_list = [[50, 100], [275, 150]]
+guidf = pd.DataFrame(gui_list)
+
+read_from_gui(1, guidf)
