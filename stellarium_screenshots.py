2b3b6b24874687442c0828bb34504442f72f9a15
AggieGirl22 <johannahein@tamu.edu>
2021-10-18 19:56:00 -0500

    edited stellarium_screenshots.py

--- stellarium_screenshots.py
+++ stellarium_screenshots.py
@@ -1,41 +1,67 @@
 import requests
 import subprocess
-import datetime
-import jdcal as j
 import pandas as pd
+import time
+
+
+def change_settings(url_main):
+    # take stellarium out of full screen mode
+    full_screen = requests.post(url_main + 'stelaction/do', data={'id': 'actionSet_Full_Screen_Global'})
+    print("Fullscreen: ", full_screen)
+
+    # remove atmosphere view
+    atmosphere = requests.post(url_main + 'stelaction/do', data={'id': 'actionShow_Atmosphere'})
+    print("Atmosphere: ", atmosphere)
+
+    # calculate julian day according to Stellarium reference
+    date = pd.to_datetime("today") + pd.Timedelta(hours=5)
+    ts = pd.Timestamp(date)
+    julian = ts.to_julian_date()
+
+    # set the date and time
+    set_date_time = {'time': julian, 'timerate': 0, }
+    time = requests.post(url_main + "main/time", data=set_date_time)
+    print("Time: ", time)
+
+    # set location
+    set_location = {'latitude': 30.6280, 'longitude': -96.3344, 'altitude': 0}
+    location = requests.post(url_main + "location/setlocationfields", data=set_location)
+    print("Location: ", location)
+
+    # set the sky view direction
+    # [south, east, altitude]
+    set_view = {'altAz': '[0.001, 0, 1]'}
+    view = requests.post(url_main + "main/view", data=set_view)
+    print("View: ", view)
+
+    # set the sky view zoom level
+    set_fov = {'fov': '190'}
+    fov = requests.post(url_main + "main/fov", data=set_fov)
+    print("FOC: ", fov)
+
+    '''requests.Response.close(fov)
+    requests.Response.close(view)
+    requests.Response.close(atmosphere)
+    requests.Response.close(location)
+    requests.Response.close(time)
+    requests.Response.close(full_screen)'''
+
+
+def take_screenshot(url_main):
+
+    # create a screenshot of the current view
+    screenshot = requests.post(url_main + 'stelaction/do', data={'id': 'actionSave_Screenshot_Global'})
+    print("Screenshot: ", screenshot)
+
+
+# enable remote control plugin for Stellarium
 # open Stellarium before running this program
+url = "http://localhost:8090/api/"
 
-url_main = "http://localhost:8090/api/"
-
-response = requests.get(url_main + "main/status")
-print("Status: ", response.status_code)
-
-# calculate julian day according to Stellarium reference
-date = pd.to_datetime("today") + pd.Timedelta(hours=5)
-ts = pd.Timestamp(date)
-julian = ts.to_julian_date()
-
-# set the date and time
-set_date_time = {'time': julian, 'timerate': 0, }
-time = requests.post(url_main + "main/time", data=set_date_time)
-print("Time: ", time)
-
-# set location
-set_location = {'latitude': 30.6280, 'longitude': -96.3344, 'name': 'College Station', 'planet': 'Earth',
-                'region': 'Northern America', 'role': 'X', 'state': 'Texas'}
-
-# set the sky view direction
-# [south, east, altitude]
-set_view = {'altAz': '[0.001, 0, 1]'}
-view = requests.post(url_main + "main/view", data=set_view)
-print("View: ", view)
-
-# set the sky view zoom level
-set_fov = {'fov': '190'}
-fov = requests.post(url_main + "main/fov", data=set_fov)
-print("FOC: ", fov)
-
-if response.status_code == 200:
-    print("Time: ", response.json().get('time'))
-    print("Location: ", response.json().get('location'))
-    print("View: ", response.json().get('view'))
+path = 'C:\\Program Files\\Stellarium\\stellarium.exe'
+proc_stellarium = subprocess.Popen(path)
+time.sleep(10)
+change_settings(url)
+time.sleep(3)
+take_screenshot(url)
+proc_stellarium.kill()
