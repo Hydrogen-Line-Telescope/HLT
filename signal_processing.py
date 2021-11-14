ed476e5bbee832e23a15cd614820ed3ede4ad397
AggieGirl22 <johannahein@tamu.edu>
2021-11-14 16:51:12 -0600

    Working on signal processing

--- signal_processing.py
+++ signal_processing.py
@@ -5,20 +5,53 @@ import pandas as pd
 
 
 def graph_data():
-    datadf = pd.read_csv('raw_signal_data.csv', header=None)
+    datadf = pd.read_csv('raw_signal_data_set.csv', header=None)
     f0 = datadf.iloc[0, 2]
     f1 = datadf.iloc[-1, 3]
     df = datadf.iloc[0, 4]
 
     sig = np.array(datadf.iloc[:, 6:])
     sig = sig.flatten()
-    freq = np.arange(f0, f1, df) / 1e6
 
-    plt.plot(freq, sig)
+    linear_sig = []
+    for i in sig:
+        new_i = np.exp(i/20)
+        linear_sig.append(new_i)
+    print(linear_sig)
+
+    freq = np.arange(f0, f1, df) / 1e9
+
+    plt.plot(freq, linear_sig)
     plt.xlim([freq[0], freq[-1]])
     plt.ylabel('PSD (dB)')
     plt.xlabel('Freq (GHz)')
     plt.show()
+
+
+def get_freq_mag():
+    datadf = pd.read_csv('raw_signal_data_set.csv', header=None)
+
+    start_freq = datadf.iloc[0, 2]
+    end_freq = datadf.iloc[-1, 3]
+    freq_interval = datadf.iloc[0, 4]
+
+    # list of signal magnitudes in dB
+    sig = np.array(datadf.iloc[:, 6:])
+    sig = sig.flatten()
+
+    # all data points in GHz
+    freq = np.arange(start_freq, end_freq, freq_interval) / 1e9
+
+    # find the peak magnitude and convert to a linear value
+    peak_mag = np.amax(sig)
+    peak_mag = np.exp(peak_mag/20)
+    max_mag_index = np.where(sig == np.amax(sig))
+    print(peak_mag)
+
+    # in GHz
+    peak_freq = freq[int(max_mag_index[0])]
+    print(peak_freq)
+
 
 
 def pull_data():
@@ -29,13 +62,17 @@ def pull_data():
     """
 
     # set a gain value to keep data consistent
-    command_line = ['-q', '-f', '1.2G', '-O', 'raw_signal_data.csv', '-g', '37.5', '-r', '6000000', '-k', '33.33']
+    command_line = ['-q', '-f', '1.2G', '-O', 'raw_signal_data_set.csv', '-g', '37.5', '-r', '6000000', '-k', '33.33',
+                    '-n', '12800']
     __main__.main(command_line)
-    # if can't fix weird noise, take more samples and cut off the weird noise parts
 
 
-pull_data()
+#pull_data()
+get_freq_mag()
 graph_data()
+
+'''pull_data()
+graph_data()'''
 
 
 '''import numpy
