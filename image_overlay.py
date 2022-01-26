ab1a2d82085e2f2df2b620ea384af66957316c99
AggieGirl22 <johannahein@tamu.edu>
2022-01-26 17:19:34 -0600

    overlaying heatmap and skymap

--- image_overlay.py
+++ image_overlay.py
@@ -15,10 +15,18 @@ def overlay_images(heatmap_file, skymap_file, coordinates_list):
     heatmap = Image.open(heatmap_file)
     skymap = Image.open(skymap_file)
     skymap_cropped = skymap.crop(coordinates_list)
+    size = skymap_cropped.size
     print(heatmap.size)
     print(skymap.size)
     print(skymap_cropped.size)
-    skymap_cropped.show()
+
+    # skymap_cropped.show()
+
+    # make heatmap and cropped skymap the same size for blending
+    heatmap = heatmap.resize(size, Image.ANTIALIAS)
+    # heatmap.show()
+    blended_image = Image.blend(skymap_cropped, heatmap, alpha=0.4)
+    blended_image.show()
 
 
 def transform_coordinates(gui_coordinates):
@@ -42,9 +50,13 @@ def transform_coordinates(gui_coordinates):
     # [left, bottom, right, top]
     feed_list = [crop_coordinates[0], crop_coordinates[3], crop_coordinates[2], crop_coordinates[1]]
 
+    # crop image larger than the user's selection
+    # how much bigger?
+    # final_list = [i + 5 for i in feed_list]
+
     return feed_list
 
 
-coordinates = [[-247, 2], [126, 214]]
+coordinates = [[-122, -68], [134, 162]]
 crop_list = transform_coordinates(coordinates)
 overlay_images("Heatmaps\\2-DSel.png", "Screenshots\\cropped_stellarium.png", crop_list)
