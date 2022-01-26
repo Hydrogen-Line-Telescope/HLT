2d17f79aa487e14aea9edb68c5a628a5030c9487
AggieGirl22 <johannahein@tamu.edu>
2022-01-26 12:37:58 -0600

    transformed coordinates for cropping

--- image_overlay.py
+++ image_overlay.py
@@ -14,24 +14,37 @@ import matplotlib.pyplot as plt
 def overlay_images(heatmap_file, skymap_file, coordinates_list):
     heatmap = Image.open(heatmap_file)
     skymap = Image.open(skymap_file)
-    skymap_cropped = skymap.crop((3, 464, 376, 124))
+    skymap_cropped = skymap.crop(coordinates_list)
     print(heatmap.size)
     print(skymap.size)
+    print(skymap_cropped.size)
     skymap_cropped.show()
 
-crop_coordinates = []
+
+def transform_coordinates(gui_coordinates):
+
+    crop_coordinates = []
+    flat_coordinates = list(np.concatenate(gui_coordinates).flat)
+    print(flat_coordinates)
+
+    coordinates_adjusted = [i + 250 for i in flat_coordinates]
+    print(coordinates_adjusted)
+
+    for i in range(len(flat_coordinates)):
+        if i % 2 == 0:
+            print(i)
+            print(coordinates_adjusted[i])
+            print(flat_coordinates[i+1])
+            crop_coordinates.append(coordinates_adjusted[i])
+            crop_coordinates.append((flat_coordinates[i+1] - 250) * -1)
+
+    # [x1, y1, x2, y2]
+    # [left, bottom, right, top]
+    feed_list = [crop_coordinates[0], crop_coordinates[3], crop_coordinates[2], crop_coordinates[1]]
+
+    return feed_list
+
+
 coordinates = [[-247, 2], [126, 214]]
-flat_coordinates = list(np.concatenate(coordinates).flat)
-print(flat_coordinates)
-
-coordinates_adjusted = [i + 250 for i in flat_coordinates]
-print(coordinates_adjusted)
-
-for i, y in enumerate(flat_coordinates):
-    if i % 2 == 0:
-        crop_coordinates.append(coordinates_adjusted[i])
-        crop_coordinates.append((y - 250) * -1)
-
-print("cropped", crop_coordinates)
-
-overlay_images("Heatmaps\\2-DSel.png", "Screenshots\\cropped_stellarium.png", coordinates_adjusted)
+crop_list = transform_coordinates(coordinates)
+overlay_images("Heatmaps\\2-DSel.png", "Screenshots\\cropped_stellarium.png", crop_list)
